5c6603b3aeda4ca234c8a215ebc007554340f216
Shatab99 <shatabag4749@gmail.com>
2025-06-13 23:06:27 +0600

    added review feature

--- src/app/modules/driverReview/driverReview.router.ts
+++ src/app/modules/driverReview/driverReview.router.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import auth from "../../Utils/auth.Middleware";
+import { UserRole } from "../../GlobalInterfaces/global.userRole";
+import validate from "../../Utils/validation.zod";
+import { driverReviewSchema } from "./driverReview.validation";
+import { driverReviewController } from "./driverReview.controller";
 
 const router = Router()
 
-router.post("/give-review", )
+router.post("/give-review", auth(UserRole.passenger), validate(driverReviewSchema), driverReviewController.giveReview)
 
 export const driverReviewRouter = router
